e081c5a07393698ba04d2d4dbd3181a27ed87ade
Pâmella <pamellakyrlatech@gmail.com>
2025-07-22 23:05:05 -0300

    Teste Cenario5-CT001 e ajuste no .gitignore

---
+++ cypress/e2e/recrutamento/CT001-cadastro-valido.cy.js
@@ -0,0 +1,19 @@
+describe('CENÁRIO 5 - Adicionar candidato para recrutamento', () => {
+    beforeEach(() => {
+      cy.login('Admin', 'admin123')
+    })
+  
+    it('CT001 - Adicionar candidato com dados válidos', () => {
+      cy.visit('/recruitment/viewCandidates')
+      cy.url().should('include', 'recruitment/viewCandidates')
+      cy.get('h5').should('contain.text', 'Candidates')
+  
+      cy.get('button').contains('Add').click()
+      cy.url().should('include', '/recruitment/addCandidate')
+      cy.adicionarCandidato()
+      cy.get('button').contains('Save').click()
+      //alternativa à mensagem de sucesso pois ela fica pouco tempo na tela para validar:
+      cy.get('p').contains('Status: Application Initiated')
+    })
+  })
+  
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,8 @@
+import { faker } from '@faker-js/faker'
+
 Cypress.Commands.add('login', (user, password) => {
-    cy.visit('/auth/login')
-    cy.get('input[name="username"]').type(user)
-    cy.get('input[name="password"]').type(password, { log: false })
-    cy.get('button[type="submit"]').click()
-  })
-  
+  cy.visit('/auth/login')
+  cy.get('input[name="username"]').type(user)
+  cy.get('input[name="password"]').type(password, { log: false })
+  cy.get('button[type="submit"]').click()
+})
