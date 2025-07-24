bc52aa5074df0fa6ef837df201c90efc6710b567
Lucas Fidelis <lucasm241301@gmail.com>
2025-07-24 09:25:17 -0300

    Ajusta formatação da data no CT001 e adiciona after para remover ponto cadastrado

--- cypress/e2e/presenca/CT001-bater-ponto-valido.cy.js
+++ cypress/e2e/presenca/CT001-bater-ponto-valido.cy.js
@@ -7,12 +7,16 @@ describe('CENÁRIO 8 - CONFIRMAR PRESENÇA DIÁRIA (“BATER PONTO”)', () => {
     cy.navigateToPunch()
   })
 
+  after(()=>{
+    cy.removePunch(dataAtual, horaEntrada)
+  })
+
+  const dataAtual = dayjs().format('YYYY-DD-MM')
+  const horaAtual = dayjs()
+  const horaEntrada = horaAtual.clone().subtract(2, 'hour').format('hh:mm A')
+  const horaSaida = horaAtual.format('hh:mm A')
+
   it('CT001 - Bater ponto com datas válidas', () => {
-    const dataAtual = dayjs().format('YYYY-MM-DD')
-    const horaAtual = dayjs()
-    const horaEntrada = horaAtual.clone().subtract(2, 'hour').format('hh:mm A')
-    const horaSaida = horaAtual.format('hh:mm A')
-  
     // Punch In
     cy.typePunchIn(dataAtual, horaEntrada, '')
     cy.clickConfirmPunchIn()
@@ -24,18 +28,15 @@ describe('CENÁRIO 8 - CONFIRMAR PRESENÇA DIÁRIA (“BATER PONTO”)', () => {
     cy.clickConfirmPunchOut()
   
     cy.wait(1000)
-  
-    const [yyyy, mm, dd] = dataAtual.split('-')
-    const dataFormatada = `${yyyy}-${dd}-${mm}`
-  
+
     cy.navigateToMyRecords()
   
     cy.reload()
   
-    cy.get('p').contains(dataFormatada, { timeout: 10000 }).should('exist')
+    cy.get('p').contains(dataAtual, { timeout: 10000 }).should('exist')
     cy.url().should('include', '/attendance/viewMyAttendanceRecord')
 
-    cy.contains(dataFormatada).should('exist')
+    cy.contains(dataAtual).should('exist')
     cy.contains(horaEntrada).should('exist')
     cy.contains(horaSaida).should('exist')
   })
